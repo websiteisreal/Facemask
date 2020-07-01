7262c013f676f11c9ed90f680b0e210fa539baae
websiteisreal <38820519+websiteisreal@users.noreply.github.com>
2020-07-01 17:05:44 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'include/connection.php';
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The Face Mask Company</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+  <h1> The Face Mask Company Website</h1>
+
+  <?php
+  $conn;
+
+
+
+
+?>
+
+<h1> Hello World</h1>
+  <ul>
+    <li> Home </li>
+    <li> <a href = "bob.php"> Contact us 
+  <ul>
+</body>
+</html>
